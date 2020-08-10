75e07bb2b4b3a698346833250c671f25f1ab16ec
Anish Duwal <duwalanise@gmail.com>
2020-08-10 13:03:40 +0545

    display captured image

--- src/camera.tsx
+++ src/camera.tsx
@@ -6,11 +6,12 @@ import {
   Ionicons,
   MaterialCommunityIcons,
 } from '@expo/vector-icons';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, Image } from 'react-native';
 
 const MyCamera: React.FC<{}> = () => {
-  const [hasPermission, setHasPermission] = useState(false);
   const cameraRef = useRef<Camera | null>(null);
+  const [hasPermission, setHasPermission] = useState(false);
+  const [photo, setPhoto] = useState<any>(null);
 
   useEffect(() => {
     const checkPermission = async () => {
@@ -23,10 +24,48 @@ const MyCamera: React.FC<{}> = () => {
   const onCapture = async () => {
     if (cameraRef) {
       let photo = await cameraRef.current?.takePictureAsync();
-      console.log(photo);
+      setPhoto(photo);
     }
   };
+  const onSave = () => {
+    console.log(photo);
+    setPhoto(null);
+  };
+  const onBack = () => {
+    setPhoto(null);
+  };
   if (!hasPermission) return null;
+
+  if (photo) {
+    return (
+      <View style={{ flex: 1 }}>
+        <Image style={{ flex: 1 }} source={{ uri: photo.uri }} />
+        <View
+          style={{
+            position: 'absolute',
+            display: 'flex',
+            bottom: 50,
+            left: 0,
+            right: 0,
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            backgroundColor: 'transparent',
+          }}
+        >
+          <TouchableOpacity onPress={onSave}>
+            <FontAwesome
+              name="upload"
+              style={{ color: '#fff', fontSize: 40 }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={onBack}>
+            <FontAwesome name="undo" style={{ color: '#fff', fontSize: 40 }} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
   return (
     <Camera style={{ flex: 1 }} type="back" ref={cameraRef}>
       <View
