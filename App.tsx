0af9a185879ee9061e6df08f453e993d2fc20592
Anish Duwal <duwalanise@gmail.com>
2020-08-12 17:49:35 +0545

    add ui-kitten design

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { configure } from 'mobx';
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import * as eva from '@eva-design/eva';
 import 'mobx-react-lite/batchingForReactNative';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { NavigationContainer } from '@react-navigation/native';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 import Main from './src/container';
 import store, { StoreContext } from './src/store';
@@ -12,16 +14,15 @@ configure({ enforceActions: 'observed' });
 
 export default function App() {
   return (
-    <StoreContext.Provider value={store}>
-      <NavigationContainer>
-        <Main />
-      </NavigationContainer>
-    </StoreContext.Provider>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <StoreContext.Provider value={store}>
+          <NavigationContainer>
+            <Main />
+          </NavigationContainer>
+        </StoreContext.Provider>
+      </ApplicationProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
